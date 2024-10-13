0dfbf29a2608df719b754f1ff2fd8dc37e317045
IvanHG <herreraleon14@gmail.com>
2024-10-12 23:23:12 -0600

    Uso de cadenas en las variables

--- backend_recetas/main.js
+++ backend_recetas/main.js
@@ -5,11 +5,11 @@ import { MongoClient } from "https://deno.land/x/mongo@v0.31.1/mod.ts";
 // Configuración de conexión a MongoDB Atlas manualmente
 const client = new MongoClient();
 await client.connect({
-  db: Deno.env.get("DB"),  // Nombre de la base de datos
+  db: `${Deno.env.get("DB")}`,  // Nombre de la base de datos
   tls: true,
   servers: [
         {
-        host: Deno.env.get("HOST"),  // Host de MongoDB
+        host: `${Deno.env.get("HOST")}`,  // Host de MongoDB
         port: 27017,
         },
     ],
@@ -17,12 +17,12 @@ await client.connect({
     username: "user",  // Usuario de MongoDB
     password: `${Deno.env.get("PASSWORD")}`,  // Contraseña de MongoDB
     db: "admin",
-    mechanism: Deno.env.get("MECHANISM"),  // Mecanismo de autenticación
+    mechanism: `${Deno.env.get("MECHANISM")}`,  // Mecanismo de autenticación
   },
 });
 
-const db = client.database(Deno.env.get("DATABASE"));
-const recetasCollection = db.collection(Deno.env.get("TABLE"));
+const db = client.database(`${Deno.env.get("DATABASE")}`);
+const recetasCollection = db.collection(`${Deno.env.get("TABLE")}`);
 
 const app = new Application();
 const router = new Router();
