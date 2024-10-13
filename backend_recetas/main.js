5fa39e44f5db738af316e732747f0ee41e1ab4db
IvanHG <herreraleon14@gmail.com>
2024-10-12 23:19:14 -0600

    Uso de Deno ENV

--- backend_recetas/main.js
+++ backend_recetas/main.js
@@ -1,30 +1,28 @@
 import { Application, Router } from "https://deno.land/x/oak@v17.1.0/mod.ts";
 import { MongoClient } from "https://deno.land/x/mongo@v0.31.1/mod.ts";
-import { config } from "https://deno.land/x/dotenv@v3.2.0/mod.ts";
 
-const env = config();
 
 // Configuración de conexión a MongoDB Atlas manualmente
 const client = new MongoClient();
 await client.connect({
-  db: env.DB,  // Nombre de la base de datos
+  db: Deno.env.get("DB"),  // Nombre de la base de datos
   tls: true,
   servers: [
         {
-        host: env.HOST,  // Host de MongoDB
+        host: Deno.env.get("HOST"),  // Host de MongoDB
         port: 27017,
         },
     ],
   credential: {
     username: "user",  // Usuario de MongoDB
-    password: env.PASSWORD,  // Contraseña de MongoDB
+    password: Deno.env.get("PASSWORD"),  // Contraseña de MongoDB
     db: "admin",
-    mechanism: env.MECHANISM,  // Mecanismo de autenticación
+    mechanism: Deno.env.get("MECHANISM"),  // Mecanismo de autenticación
   },
 });
 
-const db = client.database(env.DATABASE);
-const recetasCollection = db.collection(env.TABLE);
+const db = client.database(Deno.env.get("DATABASE"));
+const recetasCollection = db.collection(Deno.env.get("TABLE"));
 
 const app = new Application();
 const router = new Router();
